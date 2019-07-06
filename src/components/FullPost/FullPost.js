8a1e42977d8dc2571f7356c3296e1f41f3ee7dcf
damianjnc <damianjnc@gmail.com>
2019-07-06 05:20:10 +0100

    not using axios twice

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -3,18 +3,30 @@ import React, { Component } from 'react';
 import './FullPost.css';
 
 class FullPost extends Component {
+    state = {
+        selectedPosts: null
+    }
+    getProps = ()=>{
+    this.setState({selectedPosts: this.props.selectedPost})}
     render () {
-        let post = <p>Please select a Post!</p>;
-        post = (
-            <div className="FullPost">
-                <h1>Title</h1>
-                <p>Content</p>
-                <div className="Edit">
-                    <button className="Delete">Delete</button>
-                </div>
-            </div>
 
-        );
+        console.log(this.state.selectedPosts)
+        let post =<p style={{textAlign:'center'}}>Please select a Post!</p>;
+
+        if(this.props.selectedPost != null){
+            console.log(this.props.selectedPost)
+
+            post = (
+                <div className="FullPost">
+                    <h1>{this.props.selectedPost[0].title}</h1>
+                    <p>{this.props.selectedPost[0].body}</p>
+                    <div className="Edit">
+                        <button className="Delete">Delete</button>
+                    </div>
+                </div>
+
+            )}
+
         return post;
     }
 }
--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import axios from "axios";
 
 import Post from "../../components/Post/Post";
@@ -7,30 +7,65 @@ import NewPost from "../../components/NewPost/NewPost";
 import "./Blog.css";
 
 class Blog extends Component {
-  componentDidMount() {
-    axios
-      .get("https://jsonplaceholder.typicode.com/posts")
-      .then(res => console.log(response));
-    console.log(posts);
-  }
+    state = {
+        posts: [],
+        selectedPost: null,
+        shouldFetch: false
+    }
 
-  render() {
-    return (
-      <div>
-        <section className="Posts">
-          <Post />
-          <Post />
-          <Post />
-        </section>
-        <section>
-          <FullPost />
-        </section>
-        <section>
-          <NewPost />
-        </section>
-      </div>
-    );
-  }
+    componentDidMount() {
+        axios.get("https://jsonplaceholder.typicode.com/posts")
+            .then(response => {
+                const my4Posts = response.data.slice(0, 4);
+                const withAuthor = my4Posts.map(post => {
+                    return ({
+                        ...post,
+                        author: 'Damian'
+                    });
+                })
+                this.setState({posts: withAuthor})
+            });
+    }
+
+    handleClick = id => {
+        let selectedPost = this.state.posts.filter(post => id === post.id);
+
+        this.setState({shouldFetch: true, selectedPost: selectedPost});
+
+    }
+
+    render() {
+
+        /*
+               let my5Posts = [];
+               if(this.state.posts){
+                   console.log(this.state.posts[1].title)
+                   for(let i = 0; i<=4;i++){
+                       my5Posts.push(<Post  title={this.state.posts[i].title}/>)
+                   }
+               console.log(my5Posts);
+               }
+        */
+        const posts = this.state.posts.map(post => {
+            return (
+                <Post key={post.id} id={post.id} title={post.title} author={post.author} content={post.body}
+                      handleClick={this.handleClick}/>)
+        });
+        console.log(this.state.selectedPost)
+        return (
+            <div>
+                <section className="Posts">
+                    {posts}
+                </section>
+                <section>
+                    <FullPost selectedPost={this.state.selectedPost}/>
+                </section>
+                <section>
+                    <NewPost/>
+                </section>
+            </div>
+        );
+    }
 }
 
 export default Blog;
